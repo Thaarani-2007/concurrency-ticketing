60e33d9bcde587d86cd516feb632404921389966
thaarani-2007 <thaaranips007@gmail.com>
2026-05-24 19:30:32 +0530

    Add Cloudinary for media storage

--- core/settings.py
+++ core/settings.py
@@ -156,7 +156,7 @@ CORS_ALLOWED_ORIGINS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CELERY_BROKER_URL = REDIS_URL
 CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": "none"} if "upstash" in REDIS_URL else None
 CELERY_ACCEPT_CONTENT = ['json']
@@ -170,3 +170,9 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
